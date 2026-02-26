d2c44874963df80a860827639dfdb9127650b7ee
jeuryink <jeury@inkfnd.com>
2026-02-26 15:19:58 -0500

    some fixes

--- src/nado_utils/engine.rs
+++ src/nado_utils/engine.rs
@@ -13,12 +13,10 @@ use crate::eip712_structs::{
 use crate::product::Product;
 use crate::serialize_utils::{
     deserialize_bytes20, deserialize_bytes32, deserialize_i128, deserialize_nested_vec_i128,
-    deserialize_option_bytes32, deserialize_option_vec_u8,
-    deserialize_u128, deserialize_u64, deserialize_vec_i128,
-    deserialize_vec_u8, serialize_bytes20, serialize_bytes32, serialize_i128,
-    serialize_nested_vec_i128, serialize_option_bytes32,
-    serialize_option_vec_u8, serialize_u128, serialize_u64,
-    serialize_vec_i128, serialize_vec_u8, str_or_u32, WrappedI128,
+    deserialize_option_bytes32, deserialize_option_vec_u8, deserialize_u128, deserialize_u64,
+    deserialize_vec_i128, deserialize_vec_u8, serialize_bytes20, serialize_bytes32, serialize_i128,
+    serialize_nested_vec_i128, serialize_option_bytes32, serialize_option_vec_u8, serialize_u128,
+    serialize_u64, serialize_vec_i128, serialize_vec_u8, str_or_u32, WrappedI128,
 };
 use rkyv::{Archive, Deserialize as RkyvDeserialize, Serialize as RkyvSerialize};
 use serde::{Deserialize, Serialize};
